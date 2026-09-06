2bbd0fb891caf9605839cb298a1a25a8b64d10d8
Samuel Jacob <samuel.jacob9431@gmail.com>
2026-09-06 15:46:04 +0530

    feat:Car Speed added!

--- src/main.py
+++ src/main.py
@@ -7,6 +7,12 @@ WIDTH, HEIGHT = 800, 600
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Self Driving Car")
 
+#Car Positioning
+car_x = 100
+car_y = 300
+
+#Car Speed!
+speed = 4
 running = True
 
 while running:
@@ -15,6 +21,8 @@ while running:
         if event.type == pygame.QUIT:
             running = False
 
+    #Update Car position upon Speed
+    car_x += speed
     screen.fill((30,30,30)) #BG GREY (RGB Basis) 
 
     pygame.display.flip()
